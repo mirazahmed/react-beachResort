da57c2a7500364d54c7c31d6a436753c121693c1
Miraz Ahmed <miraz37@gmail.com>
2020-11-15 13:53:04 -0500

    link bug fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ export default class Navbar extends Component {
                 </div>
                 <ul className={this.state.isOpen?"nav-links show-nav":"nav-links"}>
                     <li>
-                        <Link to="/Home">Home</Link>
+                        <Link to="/">Home</Link>
                     </li>
                     <li>
                         <Link to="/rooms">Rooms</Link>
